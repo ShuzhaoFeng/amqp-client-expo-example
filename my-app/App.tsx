0dd36c9f7cf8a5a4e5a33f722f655a05c0c010b3
ShuzhaoFeng <shuzhao_feng@outlook.com>
2024-04-25 13:49:41 -0400

    Change to WebSocketClient

--- my-app/App.tsx
+++ my-app/App.tsx
@@ -1,15 +1,47 @@
-import { AMQPClient } from "@cloudamqp/amqp-client";
+import { AMQPChannel, AMQPWebSocketClient } from "@cloudamqp/amqp-client";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+
+global.TextEncoder = require("text-encoding").TextEncoder;
 
 export default function App() {
-  const cloud_amqp_url =
-    "amqps://{User & Vhost}:{password}@rabbit.lmq.cloudamqp.com/{User & Vhost}";
-  const client = new AMQPClient(cloud_amqp_url);
+  const cloud_amqp_url = "wss://rabbit-01.lmq.cloudamqp.com";
+
+  const [ready, setReady] = useState(false);
+  const [channel, setChannel] = useState<AMQPChannel | null>(null);
+
+  useEffect(() => {
+    setReady(false);
+    const client = new AMQPWebSocketClient(
+      cloud_amqp_url,
+      "vhost",
+      "password",
+      "user"
+    );
+
+    client
+      .connect()
+      .then((connection) => connection.channel())
+      .then((channel) => {
+        setChannel(channel);
+        setReady(true);
+      });
+  }, []);
 
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Button
+        title="Publish"
+        onPress={() => {
+          if (ready && channel) {
+            channel.basicPublish("", "my_queue", "Hello, World!");
+          } else {
+            console.warn("Channel Not ready");
+          }
+        }}
+      />
       <StatusBar style="auto" />
     </View>
   );
