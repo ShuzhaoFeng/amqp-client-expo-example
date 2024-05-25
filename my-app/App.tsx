69c2739b855a21eb8cdb606e2c4742aebb20729e
ShuzhaoFeng <shuzhao_feng@outlook.com>
2024-05-25 15:21:21 -0400

    Add connection logic

--- my-app/App.tsx
+++ my-app/App.tsx
@@ -1,23 +1,23 @@
 import { AMQPChannel, AMQPWebSocketClient } from "@cloudamqp/amqp-client";
 import { StatusBar } from "expo-status-bar";
 import { useEffect, useState } from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Alert, Button, StyleSheet, Text, View } from "react-native";
 
-global.TextEncoder = require("text-encoding").TextEncoder;
+import appconfig from "./appconfig.json";
 
 export default function App() {
-  const cloud_amqp_url = "wss://rabbit-01.lmq.cloudamqp.com";
-
-  const [ready, setReady] = useState(false);
+  const [ready, setReady] = useState<boolean>(false);
   const [channel, setChannel] = useState<AMQPChannel | null>(null);
+
+  const { cloud_amqp_url, cloud_amqp_user, cloud_amqp_password } = appconfig;
 
   useEffect(() => {
     setReady(false);
     const client = new AMQPWebSocketClient(
       cloud_amqp_url,
-      "vhost",
-      "password",
-      "user"
+      cloud_amqp_user,
+      cloud_amqp_password,
+      cloud_amqp_user
     );
 
     client
@@ -36,9 +36,16 @@ export default function App() {
         title="Publish"
         onPress={() => {
           if (ready && channel) {
-            channel.basicPublish("", "my_queue", "Hello, World!");
+            channel.basicPublish("", "my_queue", "Hello, World!").then(
+              () => {
+                Alert.alert("Message Sent");
+              },
+              (error) => {
+                Alert.alert("Error sending message", error.message);
+              }
+            );
           } else {
-            console.warn("Channel Not ready");
+            Alert.alert("Channel Not ready");
           }
         }}
       />
